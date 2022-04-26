cd3b2c0cb7b7e8e647398a6e6382a52b8417cca7
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-04-25 23:31:51 -0400

    style(app.tsx): bggradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import * as React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import React from 'react';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-);
+import { Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App">
+      <Box
+        w="100%"
+        h="calc(100vh)"
+        bgGradient="linear(red.100 0%, orange.100 25%, yellow.100 50%)"
+      ></Box>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import * as React from "react"
-import ReactDOM from "react-dom"
+import { ChakraProvider } from '@chakra-ui/react';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
 
-import { App } from "./App"
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
-    </ChakraProvider>    
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById("root"),
-)
+  document.getElementById('root'),
+);
