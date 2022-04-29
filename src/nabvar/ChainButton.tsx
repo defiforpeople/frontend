2bc8bd8726d95297738bc02e6300c54c495092f5
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-04-28 22:08:04 -0400

    feat(ChainButton.tsx): list with selectable chains

--- src/nabvar/ChainButton.tsx
+++ src/nabvar/ChainButton.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 import {
   Button,
-  Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
   Modal,
-  ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
@@ -18,6 +12,11 @@ import {
 } from '@chakra-ui/react';
 
 import { ReactComponent as EthereumLogo } from '../assets/logos/ethereum-logo.svg';
+import { ReactComponent as PolygonLogo } from '../assets/logos/polygon-logo.svg';
+import { ReactComponent as BnbChainLogo } from '../assets/logos/bnbchain-logo.svg';
+import { ReactComponent as AvalancheLogo } from '../assets/logos/avalanche-logo.svg';
+import { ReactComponent as FantomLogo } from '../assets/logos/fantom-logo.svg';
+
 import { TriangleDownIcon } from '@chakra-ui/icons';
 
 function ChainButton() {
@@ -34,14 +33,70 @@ function ChainButton() {
         Ehereum
       </Button>
 
-      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <Modal onClose={onClose} isOpen={isOpen} size={'sm'} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Select a Blockchain</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Texto largo de body</ModalBody>
+
+          <Button
+            leftIcon={<EthereumLogo width={25} />}
+            width={'75%'}
+            margin={'auto'}
+            marginBottom={3}
+            justifyContent="start"
+            iconSpacing={5}
+          >
+            Ethereum
+          </Button>
+
+          <Button
+            leftIcon={<PolygonLogo width={25} />}
+            width={'75%'}
+            margin={'auto'}
+            marginBottom={3}
+            justifyContent="start"
+            iconSpacing={5}
+          >
+            Polygon
+          </Button>
+
+          <Button
+            leftIcon={<BnbChainLogo width={25} />}
+            width={'75%'}
+            margin={'auto'}
+            marginBottom={3}
+            justifyContent="start"
+            iconSpacing={5}
+          >
+            BNB Chain
+          </Button>
+
+          <Button
+            leftIcon={<AvalancheLogo width={25} />}
+            width={'75%'}
+            margin={'auto'}
+            marginBottom={3}
+            justifyContent="start"
+            iconSpacing={5}
+          >
+            Avalanche
+          </Button>
+
+          <Button
+            leftIcon={<FantomLogo width={25} />}
+            width={'75%'}
+            margin={'auto'}
+            justifyContent="start"
+            iconSpacing={5}
+          >
+            Fantom
+          </Button>
+
           <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
+            <Button onClick={onClose} bg="third" color="white" variant="solid">
+              Close
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
