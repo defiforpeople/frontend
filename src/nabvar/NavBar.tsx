f16f58fe8b4987ca7cf07205bfab680f05464c4c
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-04-29 14:00:40 -0400

    style(Portfolio.tsx): change style in portfolio component

--- src/nabvar/NavBar.tsx
+++ src/nabvar/NavBar.tsx
@@ -22,8 +22,6 @@ function NavBar() {
       <Flex align="center" justify="space-between">
         <HamburgerIcon w={6} h={6} color="white" />
 
-        {/* <Logo /> */}
-
         <Heading as="h2" size="md" color="white" paddingLeft={5}>
           Defi for People
         </Heading>
