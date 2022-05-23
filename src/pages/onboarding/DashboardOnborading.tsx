48c98c611684636ed3d45372de18a3624ae08eaf
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-05-22 20:07:30 -0400

    feat: created QuestionsX components

--- src/pages/onboarding/DashboardOnborading.tsx
+++ src/pages/onboarding/DashboardOnborading.tsx
@@ -6,11 +6,16 @@ import { Step, Steps, useSteps } from 'chakra-ui-steps';
 
 import { useTranslation } from 'react-i18next';
 import '../../i18n';
-import Question from './question/Question';
+
 import IntroductionQuestions from './question/IntroductionQuestions';
+import Question1 from './question/Question1';
+import Question2 from './question/Question2';
+import Question3 from './question/Question3';
 
 function DashboardOnboarding() {
   const { t } = useTranslation('DashboardOnboarding');
+
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
 
   const steps = [
     { label: t('label1') },
@@ -23,27 +28,51 @@ function DashboardOnboarding() {
   });
 
   return (
-    <Box width={['100%', '50%', '50%']} marginTop={['50px', '100px', '100px%']}>
-      <Box>
-        <Steps
-          labelOrientation="vertical"
-          activeStep={activeStep}
-          responsive={false}
-          size={'sm'}
-        >
-          {steps.map(({ label }) => (
-            <Step label={label} key={label}>
-              {/* {content} */}
-            </Step>
-          ))}
-        </Steps>
-      </Box>
+    <Center>
+      <Box
+        width={['100%', '50%', '50%']}
+        marginTop={['50px', '100px', '100px%']}
+      >
+        <Box>
+          <Steps
+            labelOrientation="vertical"
+            activeStep={activeStep}
+            responsive={false}
+            size={'sm'}
+          >
+            {steps.map(({ label }) => (
+              <Step label={label} key={label}></Step>
+            ))}
+          </Steps>
+        </Box>
 
-      <Center>
-        <IntroductionQuestions />
-      </Center>
+        <Center>
+          {currentQuestion === 0 ? (
+            <IntroductionQuestions setQuestion={setCurrentQuestion} />
+          ) : (
+            ''
+          )}
+          {currentQuestion === 1 ? (
+            <Question1 setQuestion={setCurrentQuestion} />
+          ) : (
+            ''
+          )}
+          {currentQuestion === 2 ? (
+            <Question2
+              question={currentQuestion}
+              setQuestion={setCurrentQuestion}
+            />
+          ) : (
+            ''
+          )}
+          {currentQuestion === 3 ? (
+            <Question3 setQuestion={setCurrentQuestion} />
+          ) : (
+            ''
+          )}
+        </Center>
 
-      {/* <Center>
+        {/* <Center>
         {activeStep === steps.length ? (
           <Flex p={4}>
             <Button mx="auto" size="sm" onClick={reset}>
@@ -69,7 +98,8 @@ function DashboardOnboarding() {
           </Flex>
         )}
       </Center> */}
-    </Box>
+      </Box>
+    </Center>
   );
 }
 
