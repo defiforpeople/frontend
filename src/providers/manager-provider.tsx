fed5293eb9cfe2b95b1f0673ae3641c633fa2056
Camilo Acuña <camilo.aku@gmail.com>
2022-05-24 04:25:35 -0500

    refactor(manager-provider): remove adapter logic from manager provider

--- src/providers/manager-provider.tsx
+++ src/providers/manager-provider.tsx
@@ -1,8 +1,9 @@
-import { createContext, useMemo, useState } from 'react';
+import { createContext, useMemo } from 'react';
 import type { ReactNode } from 'react';
 import { INetworkManager } from '../utils/network-manager/manager.types';
 import { IAdapter } from '../utils/network-manager/adapters';
 import { NetworkManager } from '../utils/network-manager/manager';
+import { AdapterProvider } from './adapter-provider';
 
 type ManagerProviderProps = {
   children: ReactNode;
@@ -11,9 +12,6 @@ type ManagerProviderProps = {
 
 export type ManagerContextType = {
   manager: INetworkManager;
-  adapter: IAdapter;
-  isAuthenticated: boolean;
-  setIsAuthenticated: (value: boolean) => void;
 };
 
 export const ManagerContext = createContext<ManagerContextType | undefined>(
@@ -25,21 +23,18 @@ export function ManagerProvider({ children, adapter }: ManagerProviderProps) {
     throw new Error('adapter is not defined');
   }
 
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   const value: ManagerContextType = useMemo(() => {
     const manager = new NetworkManager();
     manager.switchAdapter(adapter);
 
     return {
       manager,
-      adapter: manager.getAdapter(),
-      isAuthenticated,
-      setIsAuthenticated,
     };
-  }, [adapter, isAuthenticated]);
+  }, [adapter]);
 
   return (
-    <ManagerContext.Provider value={value}>{children}</ManagerContext.Provider>
+    <ManagerContext.Provider value={value}>
+      <AdapterProvider manager={value.manager}>{children}</AdapterProvider>
+    </ManagerContext.Provider>
   );
 }
