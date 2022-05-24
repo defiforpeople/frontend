4decb33da70ee5e391bcfa6036c56d120b84e5db
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-05-24 09:15:00 -0400

    feat(calculateInvesment.ts): calculate invesment array

--- src/utils/calculateInvesment.ts
+++ src/utils/calculateInvesment.ts
@@ -1,13 +1,14 @@
 // Return array started with 'Hoy' and finish with 'periods' and of large periods
 function calculateInvesmentLabel(periods: number) {
   const labels = [];
-  for (let index = 0; index < periods; index++) {
+
+  for (let index = 0; index < periods + 1; index++) {
     if (index === 0) {
       labels.push('Hoy');
       continue;
     }
 
-    if (index === periods - 1) {
+    if (index === periods) {
       labels.push(`${periods} años`);
       continue;
     }
@@ -17,14 +18,46 @@ function calculateInvesmentLabel(periods: number) {
   return labels;
 }
 
-// return an array
-function calculateInvesment(periods: number) {
-  console.log(periods);
+// Return array started with initialValue and complete with monthlyAmount and of large periods
+function calculateInvesmentValues(
+  periods: number,
+  initialValue: number,
+  monthlyAmount: number,
+) {
+  const values = [];
+
+  for (let index = 0; index < periods + 1; index++) {
+    if (index === 0) {
+      values.push(initialValue);
+      continue;
+    }
+
+    values.push(values[index - 1] + monthlyAmount * 12);
+  }
+
+  return values;
+}
+
+function calculateInvesment(
+  periods: number,
+  initialValue: number,
+  monthlyAmount: number,
+) {
+  console.log(`periods: ${periods}`);
+  console.log(`initialValue: ${initialValue}`);
+  console.log(`monthlyAmount: ${monthlyAmount}`);
 
   const labels = calculateInvesmentLabel(periods);
 
+  const invested = calculateInvesmentValues(
+    periods,
+    initialValue,
+    monthlyAmount,
+  );
+
   const output = {
     labels: labels,
+    invested: invested,
   };
 
   console.log(output);
