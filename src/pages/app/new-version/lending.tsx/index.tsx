faf476b27669be301e5820bcbe02cc559020c20e
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-08-25 20:09:26 -0400

    feat: going to start to do modal for invest process

--- src/pages/app/new-version/lending.tsx/index.tsx
+++ src/pages/app/new-version/lending.tsx/index.tsx
@@ -1,16 +1,84 @@
-import { Center, Flex } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertIcon,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
 
 import FooterApp from '../../footers/FootersApp';
 import NavbarApp from '../../navbar/NavbarApp';
 import InvestSection from '../components/InvestSection';
 
+import { ReactComponent as AaveStrategy } from '../../../../assets/images/aave_strategy.svg';
+
 function Lending() {
   return (
     <Flex display={'flex'} flexDirection="column" height={'100vh'}>
       <NavbarApp />
-      <Center marginTop={'100px'}>
-        <InvestSection />
-      </Center>
+
+      <Container maxW={'3xl'} textAlign="justify">
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+          lineHeight={'110%'}
+          paddingTop={10}
+          color="third"
+        >
+          Lending protocol
+        </Heading>
+
+        <Text color={'gray.500'} paddingTop={10}>
+          This strategy provides liquidity in Aave protocol. This protocol
+          matches lenders and borrowers.
+        </Text>
+
+        <Text color={'gray.500'} paddingTop={5}>
+          It means you provide some asset for someone who needs a borrow of it,
+          and you earn a fee by what the
+          <Text as={'span'} color="#F72585" fontWeight={700}>
+            {' '}
+            borrower{' '}
+          </Text>
+          pays. You become a{' '}
+          <Text as={'span'} color="#F72585" fontWeight={700}>
+            {' '}
+            lender.{' '}
+          </Text>
+          You can withdraw the amount of the liquidity (plus the earned fees)
+          whenever you want.
+        </Text>
+
+        <Text color={'gray.500'} paddingTop={10}>
+          This strategy requires you to make 2 transactions:
+        </Text>
+
+        <Text color={'gray.500'} paddingTop={3} paddingLeft={5}>
+          1. Allow our contract to move your tokens.
+        </Text>
+
+        <Text color={'gray.500'} paddingTop={3} paddingLeft={5}>
+          2. Deposit your tokens in the Aave protocol.
+        </Text>
+
+        <Center>
+          <AaveStrategy width={'50%'} />
+        </Center>
+
+        <Center>
+          <Alert status="warning" borderRadius={10}>
+            <AlertIcon />
+            Remember, you are going to do two transacctions.
+          </Alert>
+        </Center>
+
+        <Center marginTop={'50px'}>
+          <InvestSection />
+        </Center>
+      </Container>
+
       <FooterApp />
     </Flex>
   );
